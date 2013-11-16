0090ae7ba93e7c65bf5eb3a7afb3e8e12ce70a63
nnajm <nnajm_wb@dr.project>
2013-11-16 21:52:00 +0100

    ui modifications

--- R/reporter-html.R
+++ R/reporter-html.R
@@ -23,20 +23,37 @@ HtmlReporter <- setRefClass("HtmlReporter", contains = "Reporter",
 																timer <<- proc.time()
 																result$test <- if (is.null(test) || test == "") "(unnamed)" else test
 																result$call <- if (is.null(result$call)) "(unexpected)" else result$call
-																results[[context]] <<- append(results[[context]], list(result))
+																results[[context]][[test]] <<- append(results[[context]][[test]], list(result))
+																#results[[context]] <<- append(results[[context]], list(result))
 															},
 															
 															end_reporter = function() {
-																output <- "<table>"
-																lapply(results[[context]],function(result){
+																output <- '<table cellpading="0" cellspacing="0">'
+																mapply(function(context_name, context_results){
+																	#browser()
+																	output <<- paste(output, '<tr><td><div class="runthat-context-name"> Context: ', context_name, '</div></td></tr>')
+																	mapply(function(test_name, test_results){
+																		output <<- paste(output, '<tr><td><ul><li class="runthat-test-name">&#9193;&nbsp;', test_name ,'</li>')
+																		output <<- paste(output, 
+																										 paste('<li><table class="runthat-context-results" cellpading="0" cellspacing="0">',
+																										 			'<tr><th></th><th></th><th></th><th>time</th></tr>'
+																										 ))
+																		lapply(test_results,function(result){
+																			#browser()
+																			output <<- paste(output,paste('<tr>' ,
+																																		#'<td class="runthat-checkbox"><input type="checkbox"/></td>',
+																																		'<td class="runthat-checkbox"></td>',
+																																		'<td class="runthat-result-', ifelse(result$passed,"OK","NOK"), '">', ifelse(result$passed,"&#10004;","&#10008;"),'</td>',
+																																		'<td>', ifelse(result$passed,result$success_msg, gsub("\n", " ", result$failure_msg)),'</td>',
+																																		'<td class="runthat-result-time"> ',result$time,'</td>',																																	
+																																		'</tr>',sep='')
+																											 ,sep='\n')
+																		})
+																		output <<- paste(output, '</table></li></ul></td></tr>')
+																	}, names(context_results), context_results)
 																	
-																	output <<- paste(output,paste('<tr>' ,
-																		    '<td> ',result$test,'</td>',
-																		    '<td> ',ifelse(result$passed,"OK","NO"),'</td>',
-																	     '</tr>',sep='\n'),sep='\n')
-																	
-																})
-																output <- paste(output,"</table>",sep="\n")
+																}, names(results), results)
+																output <<- paste(output,"</table>",sep="\n")
 																render <<- function()output
 # 																lapply(results[[context]],function(result){
 # 																	if (result$passed) {
